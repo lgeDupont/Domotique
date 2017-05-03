c2006c11adfecc0cb9457fd3df44953468e65882
HellNSab <serbouti.nadya@gmail.com>
2017-05-03 19:39:15 +0200

    created the design for the status box of the main room

--- DomotikApp/components/Status.js
+++ DomotikApp/components/Status.js
@@ -26,7 +26,7 @@ class Status extends Component {
     super(props)
     this.state = {
       main_room_name : 'Salon',
-      main_room_temp : '23°C'
+      main_room_temp : '23°C',
     }
   }
  
@@ -36,17 +36,22 @@ class Status extends Component {
     return (
       <View style={styles.container}><ScrollView>
         <View style={styles.main_room} ref="main_room">
-          <View style={styles.main_room_header}></View>
+          <View style={styles.main_room_header}>
+            <Text style={styles.room_names}>{this.state.main_room_name}</Text>
+          </View>
           <View style={styles.main_room_content}>
-            <View style={styles.main_room_temp}></View>
+            <View style={styles.main_room_temp}>
+              <Text style={{fontSize: 75, color: '#DBE9EE',}}>{this.state.main_room_temp}</Text>
+            </View>
             <View style={styles.main_room_commands}>
-              <View style={styles.main_room_increase}></View>
-              <View style={styles.main_room_reduce}></View>
+              <View style={styles.main_room_increase}><Text style={{fontSize: 30, color: '#DBE9EE',}}>🔼</Text></View>
+              <View style={styles.main_room_reduce}><Text style={{fontSize: 30, color: '#DBE9EE',}}>🔽</Text></View>
             </View>
           </View>
           <View style={styles.main_room_footer}>
-            <View style={styles.main_room_daytemp}></View>
-            <View style={styles.main_room_nighttemp}></View>
+            <View style={{flex: 2}}></View>
+            <View style={styles.main_room_daynighttemp}><Text>🌞   23°C</Text></View>
+            <View style={styles.main_room_daynighttemp}><Text>🌙   19°C</Text></View>
           </View>
         </View>
       </ScrollView></View>
@@ -65,33 +70,66 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#DBE9EE',
   },
+  picture: {
+    flex: 0.8,
+    width: null,
+    height: null,
+    margin: 10,
+  },
   room_names: {
-    flex: 1,
-    top:0, 
-    left: 0, 
-    right: 0, 
-    bottom: 0, 
-    marginRight : 20,
-    marginLeft: 20,
-    marginTop: 20,
-    width: width - 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#4F6D7A',
-    flexDirection:'row',
+    color: '#DBE9EE',
+    fontSize: 20,
   },
   main_room: {
     height: 150,
     width: width - 30,
-    margin : 5,
-    backgroundColor: '#DBE9EE',
+    margin : 10,
+    backgroundColor: '#114B5F',
+    flexDirection: 'column',
+  },
+  main_room_header: {
+    flex: 1,
+    margin: 5,
+  },
+  main_room_content: {
+    flex: 3,
+    flexDirection: 'row',
+    margin: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  main_room_temp: {
+    flex: 3,
+    margin: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  main_room_commands: {
+    flex: 2,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  main_room_increase:{
+    flex: 1,
+  },
+  main_room_reduce:{
+    flex: 1,
+  },
+  main_room_footer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+  },
+  main_room_daynighttemp:{
+    flex: 1,
   },
   room: {
     flex : 1,
     height: 100,
     width: (width - 30)/2,
     margin : 5,
-    backgroundColor: '#DBE9EE',
+    backgroundColor: '#114B5F',
   }
 })
 
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Domotique
+
+ToDos :
+v design of the status page
+- design of the settings page
+- design of the room-setting page
+- build node server for RaspberryPi
+- get dynamic communication between RaspberryPi and mobile app
+- add functionalities to the node server
+- switch to dynamic data on the app
+- implement the commands
+- implement extra functionalities on the app (add picture of the room, designing personilzed modes, etc)
