02b09285f4fc601249ce2a3f4eaa298983a423aa
HellNSab <serbouti.nadya@gmail.com>
2017-05-21 17:17:22 +0200

    started design of room setting design and button implementation on status tab

--- DomotikApp/components/Status.js
+++ DomotikApp/components/Status.js
@@ -10,6 +10,7 @@ import {
   Dimensions,
   StyleSheet,
   ScrollView,
+  TextInput,
   StatusBar,
   ListView,
   Image,
@@ -26,6 +27,7 @@ class Status extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      room_setting : false,
       main_room_name : 'Salon',
       main_room_temp : '23°C',
       secondary_rooms : [
@@ -97,44 +99,86 @@ class Status extends Component {
     )
   })
 }
+
+_onRoomPress(){
+  this.setState({room_setting : true})
+}
  
   render() {
     var root = this
 
     return (
-      <View style={styles.container}><ScrollView>
-        <View style={styles.main_room} ref="main_room">
-          <View style={styles.main_room_header}>
-            <Text style={styles.room_names}>{this.state.main_room_name}</Text>
-          </View>
-          <View style={styles.main_room_content}>
-            <View style={styles.main_room_temp}>
-              <Text style={{fontSize: 75, color: '#DBE9EE',}}>{this.state.main_room_temp}</Text>
+      <View style={styles.container}>
+        { !this.state.room_setting ? 
+          <ScrollView>
+          <TouchableHighlight onPress={this._onRoomPress.bind(this)}><View style={styles.main_room} ref="main_room">
+            <View style={styles.main_room_header}>
+              <Text style={styles.room_names}>{this.state.main_room_name}</Text>
             </View>
-            <View style={styles.main_room_commands}>
-              <View style={styles.main_room_increase}><Text style={{fontSize: 30, color: '#DBE9EE',}}>🔼</Text></View>
-              <View style={styles.main_room_reduce}><Text style={{fontSize: 30, color: '#DBE9EE',}}>🔽</Text></View>
-            </View>
-          </View>
-          <View style={styles.main_room_footer}>
-            <View style={{flex: 2}}></View>
-            <View style={styles.main_room_daynighttemp}><Text>🌞   23°C</Text></View>
-            <View style={styles.main_room_daynighttemp}><Text>🌙   19°C</Text></View>
-          </View>
-        </View>
-        <ListView contentContainerStyle={styles.otherrooms}
-          dataSource={this.state.ds.cloneWithRows(this.state.secondary_rooms)}
-          renderRow={(room) => 
-            <View key={room.id} style={styles.room}>
-              <View style={styles.main_room_header}>
-                <Text style={styles.room_names}>{room.name}</Text>
-              </View>
+            <View style={styles.main_room_content}>
               <View style={styles.main_room_temp}>
-                <Text style={{fontSize: 65, color: '#DBE9EE',}}>{room.temp}</Text>
+                <Text style={{fontSize: 75, color: '#DBE9EE',}}>{this.state.main_room_temp}</Text>
               </View>
-            </View>}
-        />
-      </ScrollView></View>
+              <View style={styles.main_room_commands}>
+                <View style={styles.main_room_increase}><Text style={{fontSize: 30, color: '#DBE9EE',}}>🔼</Text></View>
+                <View style={styles.main_room_reduce}><Text style={{fontSize: 30, color: '#DBE9EE',}}>🔽</Text></View>
+              </View>
+            </View>
+            <View style={styles.main_room_footer}>
+              <View style={{flex: 2}}></View>
+              <View style={styles.main_room_daynighttemp}><Text>🌞   23°C</Text></View>
+              <View style={styles.main_room_daynighttemp}><Text>🌙   19°C</Text></View>
+            </View>
+          </View></TouchableHighlight>
+          <ListView contentContainerStyle={styles.otherrooms}
+            dataSource={this.state.ds.cloneWithRows(this.state.secondary_rooms)}
+            renderRow={(room) => 
+              <View key={room.id} style={styles.room}>
+                <View style={styles.main_room_header}>
+                  <Text style={styles.room_names}>{room.name}</Text>
+                </View>
+                <View style={styles.main_room_temp}>
+                  <Text style={{fontSize: 65, color: '#DBE9EE',}}>{room.temp}</Text>
+                </View>
+              </View>}
+          />
+          </ScrollView> : null
+        }
+        { this.state.room_setting ? 
+          <View style={{flex : 1, flexDirection : 'column', width : width - 10}}>
+            <View style={styles.settings_header}>
+              <Text style={{color : '#114B5F', fontSize : 35}}>Salon</Text>
+            </View>
+            <View style={styles.settings_content}>
+              <View style={styles.settings_temp}>
+                <Text style={{color : '#114B5F', fontSize : 20, flex : 1}}>Day Temp</Text>
+                <TextInput
+                  style={{height: 30, flex : 1}}
+                  placeholder="20°C"
+                />
+              </View>
+              <View style={styles.settings_temp}>
+                <Text style={{color : '#114B5F', fontSize : 20, flex : 1}}>Night Temp</Text>
+                <TextInput
+                  style={{height: 30, flex : 1}}
+                  placeholder="18°C"
+                />
+              </View>
+              <View style={styles.settings_graph}>
+                <View style={styles.settings_graph_header}>
+                  <Text style={{color : '#114B5F', fontSize : 25}}>History</Text>
+                </View>
+                <View style={{flex : 3,}}>
+                </View> 
+              </View>
+            </View>
+            <View style={styles.settings_footer}>
+              <TouchableHighlight><Text style={{color : '#114B5F', fontSize : 20, flex : 1}}>Save Changes</Text></TouchableHighlight>
+              <TouchableHighlight><Text style={{color : '#114B5F', fontSize : 20, flex : 1}}>Delete Room</Text></TouchableHighlight>
+            </View>
+          </View> : null
+        }
+      </View>
     )
   }
 }
@@ -213,7 +257,35 @@ const styles = StyleSheet.create({
     width: (width - 30)/2,
     margin : 5,
     backgroundColor: '#114B5F',
-  }
+  }, 
+  settings_header : {
+    flex : 1,
+    justifyContent : 'center',
+    alignItems : 'center',
+  },
+  settings_footer : {
+    flex : 0.6,
+    justifyContent : 'space-around',
+    alignItems : 'center',
+    flexDirection :  'row',
+  },
+  settings_content : {
+    flex : 5,
+    flexDirection : 'column',
+    justifyContent : 'flex-start',
+  },
+  settings_temp : {
+    flex : 1,
+    flexDirection : 'row',
+    alignItems : 'stretch',
+  },
+  settings_graph : {
+    flex : 3,
+    flexDirection : 'column',
+  },
+  settings_graph_header : {
+    flex : 1,
+  },
 })
 
 export default Status
